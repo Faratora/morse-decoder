a6a9009ea66b95dfb1aac1fbb2a96fd624bb31df
Svetlana <brigantina@mail.ru>
2025-05-06 20:19:37 +0300

    Update index.js
    fix error

--- src/index.js
+++ src/index.js
@@ -37,33 +37,32 @@ const MORSE_TABLE = {
   '-----': '0',
 };
 
-//module.exports =
-function decode(expr) {
-  console.log(expr);
-  const part = expr.match(/.{1,10}/g); // Разбиваем строку на группы по 10 символов
-  console.log('Результат разбивки', part); // Результат разбивки
-  // const external = expr
-  //   .replace(/11/g, '-')
-  //   .replace(/10/g, '.')
-  //   .replace(/00/g, ' ') // убираем излишние пробелы
-  //   .replace(/\*+/g, '   ');
-  // return morseToText(external); // три пробела для разделения слов
+function morseToText(morse) {
+  return morse.map((letter) => MORSE_TABLE[letter] || ' ').join('');
 }
+
+module.exports = function decode(expr) {
+  console.log(expr);
+
+  const parts = expr.match(/.{1,10}/g); // Разбиваем строку на группы по 10 символов
+
+  console.log('Результат разбивки', parts); // Результат разбивки
+
+  const morseParts = parts.map((part) => {
+    return part
+      .replace(/11/g, '-') // Заменяем '11' на '-'
+      .replace(/10/g, '.') // Заменяем '10' на '.'
+      .replace(/\*+/g, '') // Заменяем '*' на пробел
+      .replace(/0+/g, ''); // Убираем '0'
+  });
+
+  return morseToText(morseParts);
+};
+
 const expr =
   '00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010';
-decode(expr);
+console.log(decode(expr));
 // // Функция для преобразования Морзе в текст
-// function morseToText(morse) {
-//   return morse
-//     .split('   ')
-//     .map((word) =>
-//       word
-//         .split(' ')
-//         .map((letter) => MORSE_TABLE[letter] || '')
-//         .join('')
-//     )
-//     .join(' ');
-// }
 
 // morseToText(' ');
 /* const morseCode =
